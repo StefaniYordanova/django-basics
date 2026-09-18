c05dafc7f0bc4e219e586ef0cd543124a2265e29
StefaniYordanova <stephii.y34@gmail.com>
2026-09-18 19:40:40 +0300

    added additional functionality

--- djangoTemplates/notes/models.py
+++ djangoTemplates/notes/models.py
@@ -1,3 +1,33 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    class Priorities(models.TextChoices):
+        LOW = 'Low', 'Low'
+        MEDIUM = 'Medium', 'Medium'
+        HIGH = 'High', 'High'
+
+    title = models.CharField(
+        max_length=30,
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    priority = models.CharField(
+        max_length=10,
+        choices=Priorities.choices,
+        default=Priorities.LOW,
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+    )
+    is_completed = models.BooleanField(
+        default=False,
+    )
+    due_date = models.DateField(
+        default=date.today,
+    )
--- djangoTemplates/notes/views.py
+++ djangoTemplates/notes/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Note
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'notes': Note.objects.all(),
+    }
+
+    return render(request, 'notes/index.html', context)
